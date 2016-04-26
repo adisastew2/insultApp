91b61ec6de278683cc39b6f34143d8d2c9047260
Adisa Stewart <adisa.stewart1988@gmail.com>
2016-04-25 21:16:13 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// popover funtionality
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
+});
+
+// adj1 array
+var adjOne = []
+
+// adj2 array
+var adjTwo = []
+
+// noun array
+var noun = []
+
